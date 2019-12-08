d55ea7ffecb76419169244cc954866ecb049c232
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2019-12-08 11:51:22 -0600

    create prototype-1.js and prototype-one.html

---
+++ js/prototype-1.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let form = document.getElementById("formOne");
+    // debugger;
+
+    form.addEventListener("submit", () => {
+        debugger;
+        let formData = new FormData(form);
+        console.log(formData.toString());
+        debugger;
+
+    });
+});
+
